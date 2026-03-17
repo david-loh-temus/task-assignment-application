d0ea4f01edf93ec51713cd56f696465a1a85256e
David Loh <131421744+loh-david@users.noreply.github.com>
2026-03-17 21:54:44 +0800

    chore: add baseline express app structure

---
+++ backend/src/config/config.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DEFAULT_PORT = 4000;
+const DEFAULT_LOG_LEVEL = 'info';
+const PARSE_INT_RADIX = 10;
+
+const nodeEnv = process.env.NODE_ENV ?? 'development';
+const parsedPort = Number.parseInt(process.env.PORT ?? `${DEFAULT_PORT}`, PARSE_INT_RADIX);
+
+const config = {
+  logLevel: process.env.LOG_LEVEL ?? (nodeEnv === 'test' ? 'silent' : DEFAULT_LOG_LEVEL),
+  nodeEnv,
+  port: Number.isNaN(parsedPort) ? DEFAULT_PORT : parsedPort,
+} as const;
+
+export default config;
